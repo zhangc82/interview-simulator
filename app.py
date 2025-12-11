7329583b4c5c4beda59dd1aee33e844a3f2f09fe
Chen Zhang <zhangc@mymacbook.local>
2025-12-11 10:41:01 -0500

    Updated title

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from openai import OpenAI
 import streamlit as st
 
 st.set_page_config(page_title="Streamlit Chat", page_icon="")
-st.title("Chatbot")
+st.title("Interview Simulator")
 
 if "setup_complete" not in st.session_state:
     st.session_state.setup_complete = False
